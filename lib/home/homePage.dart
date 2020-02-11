3cb704154b64c56d8fd50717995740ed9237d9dc
acengic2 <axc2629@g.rit.edu>
2020-02-11 12:33:06 +0100

    singleChildScrollView added

--- lib/home/homePage.dart
+++ lib/home/homePage.dart
@@ -24,56 +24,58 @@ class _SignUpState extends State<SignUp> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyColor().black,
-      body: Center(
-        child: Container(
-          margin: EdgeInsets.only(top: 180.0),
-          child: Column(
-            children: <Widget>[
-              Center(
-                  child: Text(
-                MyText().headline,
-                style: TextStyle(fontSize: 60, color: MyColor().white),
-              )),
-              Center(
-                  child: Text(
-                MyText().subtitle,
-                style: TextStyle(fontSize: 20, color: MyColor().white),
-              )),
-              Container(
-                width: 255.0,
-                margin: EdgeInsets.only(top: 130.0),
-                child: RaisedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed(Register);
-                    },
-                    child: Text(MyText().btnSU)),
-              ),
-              Container(
-                width: 255.0,
-                child: RaisedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed(Login);
-                    },
-                    child: Text(MyText().btnSI)),
-              ),
-              Container(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            margin: EdgeInsets.only(top: 180.0),
+            child: Column(
+              children: <Widget>[
+                Center(
+                    child: Text(
+                  MyText().headline,
+                  style: TextStyle(fontSize: 60, color: MyColor().white),
+                )),
+                Center(
+                    child: Text(
+                  MyText().subtitle,
+                  style: TextStyle(fontSize: 20, color: MyColor().white),
+                )),
+                Container(
                   width: 255.0,
-                  margin: EdgeInsets.only(top: 10.0),
-                  child: Center(
-                      child: FlatButton(
-                          onPressed: () => Navigator.of(context).pushNamed(
-                              Dashboard,
-                              arguments: PasswordArguments(
-                                  email: '',
-                                  password: '',
-                                  phone: '',
-                                  username: '')),
-                          child: Text(
-                            MyText().skipThisStep,
-                            style: TextStyle(
-                                color: MyColor().white, fontSize: 18.0),
-                          )))),
-            ],
+                  margin: EdgeInsets.only(top: 130.0),
+                  child: RaisedButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamed(Register);
+                      },
+                      child: Text(MyText().btnSU)),
+                ),
+                Container(
+                  width: 255.0,
+                  child: RaisedButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamed(Login);
+                      },
+                      child: Text(MyText().btnSI)),
+                ),
+                Container(
+                    width: 255.0,
+                    margin: EdgeInsets.only(top: 10.0),
+                    child: Center(
+                        child: FlatButton(
+                            onPressed: () => Navigator.of(context).pushNamed(
+                                Dashboard,
+                                arguments: PasswordArguments(
+                                    email: '',
+                                    password: '',
+                                    phone: '',
+                                    username: '')),
+                            child: Text(
+                              MyText().skipThisStep,
+                              style: TextStyle(
+                                  color: MyColor().white, fontSize: 18.0),
+                            )))),
+              ],
+            ),
           ),
         ),
       ),
