a351a3849ee7a203aa266c7128017ef1d3830c24
Rijad Babovic <rijad.babovic@hotmail.com>
2020-02-11 12:31:13 +0100

    verifyPinScreen

--- lib/components/myText.dart
+++ lib/components/myText.dart
@@ -21,6 +21,14 @@ class MyText {
   final String labelUsername = 'Username';
   final String loginHeadline = 'Enter username & password to login';
   final String skipThisStep = 'skip this step';
+  final String verifyPageHeadline1 = 'Verify your';
+  final String verifyPageHeadline2 = 'number';
+  final String verifyMoney1 = '2';
+  final String verifyMoney2 = 'SAR';
+  final String verifyEnterPin = 'Enter the pin';
+  final String verifyRecieve = "didn't recieve the pin";
+
+
 
 
   /// snackbar texts
