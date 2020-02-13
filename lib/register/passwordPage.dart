f4028e561d32e0d3444eb43ced9ce68497888c5a
Cikic Sena <cikicsena@gmail.com>
2020-02-13 12:03:38 +0100

    password page fix

--- lib/register/passwordPage.dart
+++ lib/register/passwordPage.dart
@@ -1,14 +1,13 @@
 import 'dart:async';
 import 'package:fillproject/components/MyText.dart';
 import 'package:fillproject/components/myColor.dart';
-import 'package:fillproject/components/myTextFormField.dart';
 import 'package:fillproject/components/myValidation.dart';
 import 'package:fillproject/firebaseMethods/firebaseCrud.dart';
 import 'package:fillproject/routes/routeArguments.dart';
 import 'package:fillproject/routes/routeConstants.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-// import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 String password;
 int _btnCounter = 0;
@@ -33,118 +32,121 @@ class PasswordPage extends StatelessWidget {
         backgroundColor: MyColor().black,
         centerTitle: true,
       ),
-      resizeToAvoidBottomInset: false,
       backgroundColor: MyColor().black,
       body: Builder(
-        builder: (context) => Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 28, bottom: 35),
-                child: Text(MyText().passwordHeadline,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 23,
-                      color: MyColor().white,
-                    )),
-              ),
-              Text(MyText().fiveSar,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 40,
-                    color: MyColor().white,
-                  )),
-              Container(
-                  height: 83,
-                  width: 316,
-                  margin: EdgeInsets.only(bottom: 19, top: 28),
-                  child: Form(
-                    key: _formKey,
-                    child: TextFormField(
-                      controller: passwordController,
-                      decoration: InputDecoration(
-                        contentPadding: new EdgeInsets.symmetric(
-                            vertical: 25.0, horizontal: 10.0),
-                        labelText: MyText().labelPassword,
-                        labelStyle: TextStyle(color: MyColor().white),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                          borderSide: BorderSide(color: MyColor().white),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                          borderSide: BorderSide(color: MyColor().white),
-                        ),
-                        focusedErrorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                          borderSide: BorderSide(
-                            color: MyColor().error,
+        builder: (context) => new GestureDetector(
+                  child: SingleChildScrollView(
+                    child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 28, bottom: 35),
+                    child: Text(MyText().passwordHeadline,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 23,
+                          color: MyColor().white,
+                        )),
+                  ),
+                  Text(MyText().fiveSar,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: MyColor().white,
+                      )),
+                  Container(
+                      height: 83,
+                      width: 316,
+                      margin: EdgeInsets.only(bottom: 19, top: 28),
+                      child: Form(
+                        key: _formKey,
+                        child: TextFormField(
+                          controller: passwordController,
+                          decoration: InputDecoration(
+                            contentPadding: new EdgeInsets.symmetric(
+                                vertical: 25.0, horizontal: 10.0),
+                            labelText: MyText().labelPassword,
+                            labelStyle: TextStyle(color: MyColor().white),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                              borderSide: BorderSide(color: MyColor().white),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                              borderSide: BorderSide(color: MyColor().white),
+                            ),
+                            focusedErrorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                              borderSide: BorderSide(
+                                color: MyColor().error,
+                              ),
+                            ),
+                            errorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                              borderSide: BorderSide(
+                                color: MyColor().error,
+                              ),
+                            ),
                           ),
+                          style: TextStyle(color: MyColor().white),
+                          obscureText: true,
+                          validator: (password) =>
+                              MyValidation().validatePassword(password),
                         ),
-                        errorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                          borderSide: BorderSide(
-                            color: MyColor().error,
-                          ),
+                      )),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 21, left: 43, right: 43),
+                    width: 316.0,
+                    child: RichText(
+                      text: new TextSpan(children: [
+                        new TextSpan(
+                          text: MyText().passwordSubtitle1,
+                          style: new TextStyle(color: Colors.white, fontSize: 12),
                         ),
-                      ),
-                      style: TextStyle(color: MyColor().white),
-                      obscureText: true,
-                      validator: (password) =>
-                          MyValidation().validatePassword(password),
+                        new TextSpan(
+                            text: MyText().privacy,
+                            style: new TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12),
+                            recognizer: new TapGestureRecognizer()
+                              ..onTap = () {
+                                 launch('https://google.com');
+                              }),
+                        new TextSpan(
+                          text: MyText().passwordSubtitle2,
+                          style: new TextStyle(color: Colors.white, fontSize: 12),
+                        ),
+                        new TextSpan(
+                            text: MyText().termsOfService,
+                            style: new TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12),
+                            recognizer: new TapGestureRecognizer()
+                              ..onTap = () {
+                                 launch('https://google.com');
+                              })
+                      ]),
                     ),
-                  )),
-              Container(
-                margin: EdgeInsets.only(bottom: 21, left: 43, right: 43),
-                width: 316.0,
-                child: RichText(
-                  text: new TextSpan(children: [
-                    new TextSpan(
-                      text: MyText().passwordSubtitle1,
-                      style: new TextStyle(color: Colors.white, fontSize: 12),
-                    ),
-                    new TextSpan(
-                        text: MyText().privacy,
-                        style: new TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 12),
-                        recognizer: new TapGestureRecognizer()
-                          ..onTap = () {
-                            // launch('https://google.com');
-                          }),
-                    new TextSpan(
-                      text: MyText().passwordSubtitle2,
-                      style: new TextStyle(color: Colors.white, fontSize: 12),
-                    ),
-                    new TextSpan(
-                        text: MyText().termsOfService,
-                        style: new TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 12),
-                        recognizer: new TapGestureRecognizer()
-                          ..onTap = () {
-                            // launch('https://google.com');
-                          })
-                  ]),
-                ),
+                  ),
+                  Container(
+                    width: 316.0,
+                    height: 67,
+                    child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(33.5),
+                        ),
+                        onPressed: () {
+                          onPressed(context);
+                        },
+                        child: Text(MyText().btnPassword, style: TextStyle(fontSize: 18)))
+                  ),
+                ],
               ),
-              Container(
-                width: 316.0,
-                height: 67,
-                child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(33.5),
-                    ),
-                    onPressed: () {
-                      onPressed(context);
-                    },
-                    child: Text(MyText().btnPassword, style: TextStyle(fontSize: 18)))
-              ),
-            ],
+            ),
           ),
         ),
       ),
