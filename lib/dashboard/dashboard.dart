5f603764c7671afaaf0c857c3d654960b6a190e8
Cikic Sena <cikicsena@gmail.com>
2020-02-18 16:32:20 +0100

    sar, questions and answers - reading from firestore

--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:fillproject/components/MyText.dart';
 import 'package:fillproject/components/myColor.dart';
+import 'package:fillproject/firebaseMethods/firebaseCheck.dart';
 import 'package:fillproject/localStorage/loginStorage.dart';
 import 'package:fillproject/routes/routeArguments.dart';
 import 'package:fillproject/routes/routeConstants.dart';
@@ -20,49 +21,101 @@ class _DashboardPageState extends State<DashboardPage> {
   final PasswordArguments arguments;
   bool isLoggedIn = false;
   String name = '';
+  int sar;
+  String question;
+  List<dynamic> choices;
+  List<dynamic> choicesEnd;
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseCheck().getQuestions();
+  }
 
   _DashboardPageState({this.arguments});
-  
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        backgroundColor: MyColor().black,
-        appBar: new AppBar(
-          title: new Text(MyText().appBarDash),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Container(
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new Text(MyText().dashHeadline,
-                    style: TextStyle(color: MyColor().white)),
-                SizedBox(
-                  height: 15.0,
-                ),
-                Text(
-                    MyText().dashSub + " ${arguments.email}"  +
-                        
-                        MyText().dashSub1  + "${arguments.password}" + 
-                        " Moj telefon je ${arguments.phone}" + 
-                        "MOJ USERNAME JE: " + "${arguments.username}",
-                    
-                    style: TextStyle(color: MyColor().white)),
-                SizedBox(
-                  height: 15.0,
-                ),
-                new OutlineButton(
-                  borderSide: BorderSide(
-                      color: Colors.red, style: BorderStyle.solid, width: 3.0),
-                  child: Text(MyText().btnLogout,
-                      style: TextStyle(color: MyColor().white)),
-                  onPressed: () => onPressed(context),
-                ),
-              ],
-            ),
+    return Scaffold(
+      backgroundColor: MyColor().black,
+      appBar: new AppBar(
+        title: new Text(MyText().appBarDash),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          FutureBuilder(
+            future: FirebaseCheck().getQuestions(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    question = snapshot.data[index].data['title'];
+                    sar = snapshot.data[index].data['sar'];
+                   choices = snapshot.data[index].data['choices'];
+                   List<dynamic> choiseOne = [];
+                    for (int i = 0; i < choices.length; i++) {
+                      choiseOne.add(choices[i]['text']);
+                    }
+                    return Container(
+                      child: new Column(
+                        children: <Widget>[
+                          new Text('Sar $sar',
+                              style: TextStyle(color: MyColor().white)),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          Text('Pitanje $question',
+                              style: TextStyle(color: MyColor().white)),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          new Column(
+                              children: choiseOne.map((item) => Container(
+                                child: Text(item,style: TextStyle(color: MyColor().white)),
+                              )).toList(), 
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          new OutlineButton(
+                            borderSide: BorderSide(
+                                color: Colors.red,
+                                style: BorderStyle.solid,
+                                width: 3.0),
+                            child: Text(MyText().btnLogout,
+                                style: TextStyle(color: MyColor().white)),
+                            onPressed: () => onPressed(context),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              }
+              return SizedBox();
+            },
           ),
-        ));
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0, // this will be set when a new tab is tapped
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.speaker_notes),
+            title: new Text(''),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.view_carousel),
+            title: new Text(''),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), title: Text(''))
+        ],
+      ),
+    );
   }
 
   onPressed(BuildContext context) {
--- lib/firebaseMethods/firebaseCheck.dart
+++ lib/firebaseMethods/firebaseCheck.dart
@@ -3,9 +3,6 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirebaseCheck {
-
-
-
   /// provjera da li username postoji u bazi
   ///
   Future<bool> doesNameAlreadyExist(String username) async {
@@ -54,8 +51,8 @@ class FirebaseCheck {
     return documents.length == 1;
   }
 
-/// pomocu metode [getUser] na osnovu unesenog emaila u resetPassword kupimo usera
-/// i updatujemo password
+  /// pomocu metode [getUser] na osnovu unesenog emaila u resetPassword kupimo usera
+  /// i updatujemo password
   Future getUser(String email) async {
     var firestore = Firestore.instance;
     QuerySnapshot qn = await firestore
@@ -65,5 +62,9 @@ class FirebaseCheck {
     return qn.documents;
   }
 
-  
+  Future getQuestions() async {
+    var firestore = Firestore.instance;
+    QuerySnapshot qn = await firestore.collection('Questions').getDocuments();
+    return qn.documents;
+  }
 }
