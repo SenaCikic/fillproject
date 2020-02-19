4b257bcd98a33601be31815a6688eff6990361b7
Cikic Sena <cikicsena@gmail.com>
2020-02-19 13:35:14 +0100

    showing fields dynamic

--- lib/components/myCardMCQ.dart
+++ lib/components/myCardMCQ.dart
@@ -6,15 +6,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyCardMCQ extends StatelessWidget {
-  final String text;
-  MyCardMCQ({this.text});
+  final String question;
+  final int sar;
+  final List<String> choices = ['jedan', 'dva', 'tri'];
+
+  MyCardMCQ({this.sar, this.question});
 
   @override
   Widget build(BuildContext context) {
     return Container(
         width: ScreenUtil.instance.setWidth(336.0),
         height: ScreenUtil.instance.setHeight(490.0),
-        margin: EdgeInsets.only(bottom: 29.0),
+        margin: EdgeInsets.only(bottom: 29.0, right: 15, left: 20),
         decoration: BoxDecoration(
             borderRadius: BorderRadius.all(Radius.circular(30)),
             color: MyColor().black),
@@ -23,36 +26,18 @@ class MyCardMCQ extends StatelessWidget {
               top: 98,
               start: 49,
               child: MyQuestion(
-                question:
-                    'This part is for questionsThis part is for questionsThis ?',
+                question: question,
               )),
           PositionedDirectional(
-              top: 41, start: 41, child: MyQuestionSAR(text: '50 SAR')),
+              top: 41, start: 41, child: MyQuestionSAR(text: '$sar SAR')),
           PositionedDirectional(
               top: 180,
               start: 41,
               child: Container(
-                height: ScreenUtil.instance.setHeight(260.0),
-                width: ScreenUtil.instance.setWidth(260.0),
-                  child: ListView(
-                    children: <Widget>[
-                      MyMCQChoice(
-                        choice: 'Prvi odgovor',
-                      ),
-                      MyMCQChoice(
-                        choice: 'Drugi odgovor',
-                      ),
-                      MyMCQChoice(
-                        choice: 'Treći odgovor',
-                      ),
-                      MyMCQChoice(
-                        choice: 'Četvrti odgovor',
-                      ),
-                      MyMCQChoice(
-                        choice: 'Peti odgovor',
-                      ),
-                    ],
-                  ),
+                                child: ListView(
+                    children: choices
+                        .map((item) => MyMCQChoice(choice: item))
+                        .toList()),
               ))
         ]));
   }
