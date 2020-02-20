209cab4dffedd3f9f8cadaebd5fa7661e3008d95
Cikic Sena <cikicsena@gmail.com>
2020-02-20 11:29:43 +0100

    showing cards by type

--- lib/components/myCardYesNo.dart
+++ lib/components/myCardYesNo.dart
@@ -6,15 +6,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyCardYesNo extends StatelessWidget {
-  final String text;
-  MyCardYesNo({this.text});
+  final String question;
+  int sar;
+  MyCardYesNo({this.question, this.sar});
 
   @override
   Widget build(BuildContext context) {
     return Container(
         width: ScreenUtil.instance.setWidth(336.0),
         height: ScreenUtil.instance.setHeight(320.0),
-        margin: EdgeInsets.only(bottom: 29.0),
+        margin: EdgeInsets.only(bottom: 29.0, left: 15, right: 15, top:180),
         decoration: BoxDecoration(
             borderRadius: BorderRadius.all(Radius.circular(30)),
             color: MyColor().black),
@@ -24,7 +25,7 @@ class MyCardYesNo extends StatelessWidget {
               start: 49,
               child: MyQuestion(
                 question:
-                    'This part is for questionsThis part is for questionsThis ?',
+                    question,
               )),
           PositionedDirectional(
               top: 41, start: 41, child: MyQuestionSAR(text: '50 SAR')),
--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -1,7 +1,10 @@
-import 'package:fillproject/components/MyText.dart';
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fillproject/components/emptyCont.dart';
 import 'package:fillproject/components/myCardMCQ.dart';
+import 'package:fillproject/components/myCardYesNo.dart';
 import 'package:fillproject/components/myCashBalance.dart';
-import 'package:fillproject/components/myColor.dart';
 import 'package:fillproject/components/mySAR.dart';
 import 'package:fillproject/firebaseMethods/firebaseCheck.dart';
 import 'package:fillproject/models/questionModel.dart';
@@ -9,6 +12,7 @@ import 'package:fillproject/routes/routeArguments.dart';
 import 'package:flutter/material.dart';
 
 bool visible = false;
+DocumentSnapshot snap;
 
 class DashboardPage extends StatefulWidget {
   final PasswordArguments arguments;
@@ -24,73 +28,110 @@ class _DashboardPageState extends State<DashboardPage> {
   final PasswordArguments arguments;
   bool isLoggedIn = false;
 
-  String name = '';
-  int sar;
-  String question;
+  int sar, userLevel;
+  String id, question, type;
   List<dynamic> choices;
   List<dynamic> snapi = [];
-
-  @override
-  void initState() {
-    super.initState();
-    FirebaseCheck().getQuestions();
-  }
+  DocumentSnapshot snap;
 
   _DashboardPageState({this.arguments});
+
+ 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            MyCashBalance(text: 'Your cash\tbalance'),
-            MySAR(text: ' 5\tSAR'),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: SizedBox(
-                    height: 500,
-                    child: FutureBuilder(
-                      future: FirebaseCheck().getQuestions(),
-                      builder: (BuildContext context, AsyncSnapshot snapshot) {
-                        if (snapshot.hasData) {
-                          if (!visible) {
-                            snapi = snapshot.data
-                                .map((doc) => Question.fromDocument(doc))
-                                .toList();
-                            visible = true;
+      body: WillPopScope(
+        onWillPop: _onWillPop,
+        child: Center(
+          child: ListView(
+            children: <Widget>[
+              /// [getUsername] - pomocu ove metode uzimamo sve itne odatke od usera
+              ///
+              /// userID [id] i userov level [userLevel]
+              FutureBuilder(
+                future: FirebaseCheck().getUserUsername(arguments.username),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder (
+                        shrinkWrap: true,
+                        physics: ClampingScrollPhysics(),
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (context, index) {
+                          snap = snapshot.data[index];
+                          id = snap.data['user_id'];
+                          userLevel = snap.data['level'];
+                          print('User je: ' +
+                              id +
+                              " , a level je = " +
+                              userLevel.toString());
+                          return EmptyContainer();
+                        });
+                  }
+                  return EmptyContainer();
+                },
+              ),
+              MyCashBalance(text: 'Your cash\tbalance'),
+              MySAR(text: ' 5\tSAR'),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: SizedBox(
+                      height: 500,
+                      child: FutureBuilder(
+                        future: FirebaseCheck().getQuestions(userLevel),
+                        builder:
+                            (BuildContext context, AsyncSnapshot snapshot) {
+                          if (snapshot.hasData) {
+                            print('ovdje sam $userLevel');
+
+                            /// punjenje lokalnog niza
+                            ///
+                            /// nakon sto se jednom napuni nepuni se vise
+                            /// visible se seta na true
+                            if (!visible) {
+                              snapi = snapshot.data
+                                  .map((doc) => Question.fromDocument(doc))
+                                  .toList();
+                              visible = true;
+                            }
+                            return ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              shrinkWrap: true,
+                              itemCount: snapi.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                choices = snapi[index].choices;
+                                sar = snapi[index].sar;
+                                question = snapi[index].title;
+                                type = snapi[index].type;
+                                return GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      snapi.removeAt(index);
+                                    });
+                                  },
+                                  child: type=='checkbox' ? 
+                                  new MyCardMCQ(
+                                      sar: sar,
+                                      question: question,
+                                      choices: choices) : MyCardYesNo(
+                                        sar: sar,
+                                        question: question
+                                      )
+                                );
+                              },
+                            );
                           }
-                          return ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            shrinkWrap: true,
-                            itemCount: snapi.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              choices = snapi[index].choices;
-                              sar = snapi[index].sar;
-                              question = snapi[index].title;
-                              print('AAAAAAAAAAAAAAAA $choices');
-                              return GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    snapi.removeAt(index);
-                                  });
-                                },
-                                child:
-                                    new MyCardMCQ(sar: sar, question: question, choices: choices),
-                              );
-                            },
-                          );
-                        }
-                        return CircularProgressIndicator();
-                      },
+                          return CircularProgressIndicator();
+                        },
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -111,9 +152,24 @@ class _DashboardPageState extends State<DashboardPage> {
     );
   }
 
-  onPressed(BuildContext context, int index) {
-    setState(() {
-      snapi.removeAt(index);
-    });
+  Future<bool> _onWillPop() async {
+    return showDialog(
+          context: context,
+          builder: (context) => new AlertDialog(
+            title: new Text('Are you sure?'),
+            content: new Text('Do you want to exit the app?'),
+            actions: <Widget>[
+              new FlatButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: new Text('No'),
+              ),
+              new FlatButton(
+                onPressed: () => exit(0),
+                child: new Text('Yes'),
+              ),
+            ],
+          ),
+        ) ??
+        true;
   }
 }
